daf4c23b52f102e1973393db2798c764cb866aa6
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2023-01-12 12:55:19 +0200

    add file JS , update 03_JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
 
+import debounce from "lodash.debounce";
+
+import { fetchCountries } from './fetchCountries.js';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', debounce(onInputMessage, DEBOUNCE_DELAY));
+
+function onInputMessage(evt) {
+    evt.preventDefault();
+
+    let inputCountry = evt.target.value.trim();
+    
+
+}
+
+// const response = fetch('https://restcountries.com/v3.1/all?fields=name.official,capital,population,flags.svg,languages')
+
+// response.then(resp => resp.json()).then(data => console.log(data))
